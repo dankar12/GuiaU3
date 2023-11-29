d55c078669408c089694bd7d8f9a621a5e82bad8
dankar12 <143622228+dankar12@users.noreply.github.com>
2023-11-28 18:31:51 -0600

    Add files via upload

---
+++ guia13.java
@@ -0,0 +1,31 @@
+
+public class guia13 {
+	
+	
+		public static final byte EDAD = 25;
+    public static final short DISTANCIA = 10000;
+    public static final int CANTIDAD = 500000;
+    public static final long POBLACION_MUNDIAL = 7800000000L;
+    public static final float ALTURA = 1.75f;
+    public static final double SALARIO = 55000.50;
+    public static final char INICIAL = 'A';
+    public static final boolean ES_ESTUDIANTE = true;
+
+    // Tipo de dato no primitivo (String) constante
+    public static final String NOMBRE = "Juan";
+
+    public static void main(String[] args) {
+        // Imprimir los valores de las constantes
+        System.out.println("Edad: " + EDAD);
+        System.out.println("Distancia: " + DISTANCIA);
+        System.out.println("Cantidad: " + CANTIDAD);
+        System.out.println("Población Mundial: " + POBLACION_MUNDIAL);
+        System.out.println("Altura: " + ALTURA);
+        System.out.println("Salario: " + SALARIO);
+        System.out.println("Inicial: " + INICIAL);
+        System.out.println("¿Es estudiante?: " + ES_ESTUDIANTE);
+        System.out.println("Nombre: " + NOMBRE);
+    }
+}
+
+	
