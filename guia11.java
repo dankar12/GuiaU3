b49f6d09e57388cde35f914241e1105acd69c8c2
dankar12 <143622228+dankar12@users.noreply.github.com>
2023-11-28 18:19:07 -0600

    Add files via upload

---
+++ guia11.java
@@ -0,0 +1,30 @@
+
+public class guia11 {
+	
+	public static void main(String[] args) {
+		// Tipos de datos primitivos
+        byte edad = 19;
+        short distancia = 20000;
+        int cantidad = 100000;
+        long poblacionMundial = 7800000000L; // El sufijo 'L' indica que es de tipo long
+        float altura = 1.75f; // El sufijo 'f' indica que es de tipo float
+        double salario = 18000.00;
+        char inicial = 'D';
+        boolean esEstudiante = true;
+
+        // Tipo de dato no primitivo (String)
+        String nombre = "Daniel";
+
+        // Imprimir los valores de las variables
+        System.out.println("Edad: " + edad);
+        System.out.println("Distancia: " + distancia);
+        System.out.println("Cantidad: " + cantidad);
+        System.out.println("Población Mundial: " + poblacionMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salario: " + salario);
+        System.out.println("Inicial: " + inicial);
+        System.out.println("¿Es estudiante?: " + esEstudiante);
+        System.out.println("Nombre: " + nombre);
+    }
+}
+	
